f47766fadee679a6c22f54bbe37ac33a1cce24a8
Nikolai Grigoriev <2364632+ngrigoriev@users.noreply.github.com>
2024-10-28 21:02:04 -0400

    switch from QMK hjson files to the API

--- keyboards/handwired/dactyl_manuform/5x7_2_6/keymaps/northstar/extras/gen_kle_layout.py
+++ keyboards/handwired/dactyl_manuform/5x7_2_6/keymaps/northstar/extras/gen_kle_layout.py
@@ -6,6 +6,8 @@ import argparse
 import csv
 import os
 import jq
+from datetime import timedelta
+from requests_cache import CachedSession
 
 
 def parse_json_file(file_path, expected_type):
@@ -48,16 +50,35 @@ def update_keys(kle_template, layer_it, kc_dict):
     return result
 
 
-def load_qmk_keycodes(dir, kc_dict):
-    jq_prog = jq.compile('.keycodes // {} | to_entries | [.[] | select (.value | type == "object")] | map({key: .value.key, label: .value.label, aliases: (.value.aliases + [.value.key] // [.value.key])}) | map({alias: .aliases | .[], label: .label}) | .[]')
-    for kc_fn in os.listdir(dir):
-        f = os.path.join(dir, kc_fn)
-        if os.path.isfile(f) and (os.stat(f).st_size > 2):
-            qmk_dict = parse_json_file(f, dict)
-            qmk_dict_transfomed = jq_prog.input_value(qmk_dict)
-            for qmk_kc in iter(qmk_dict_transfomed):
-                if 'label' in qmk_kc:
-                    kc_dict[qmk_kc['alias']] = "\n\n\n\n\n\n\n\n\n" + str(qmk_kc['label']) + "\n\n"
+def load_qmk_keycodes_api(kc_dict):
+    jq_index_prog = jq.compile('.constants | to_entries | map({version: .value | .[], file: .key}) | .[] | "/" + .file + "_" + .version + ".json" ')
+    jq_prog = jq.compile('(.keycodes // {}) + (.aliases // {}) | to_entries | [.[] | select (.value | type == "object")] | map({key: .value.key, label: .value.label, aliases: (.value.aliases + [.value.key] // [.value.key])}) | map({alias: .aliases | .[], label: .label}) | .[]')
+
+    API_URL = "https://keyboards.qmk.fm/v1"
+    API_METADATA_URL = API_URL + "/constants_metadata.json"
+
+    session = CachedSession(
+        'qmk_api_cache',
+        use_cache_dir=True,
+        cache_control=True,
+        expire_after=timedelta(days=1),
+        allowable_codes=[200],
+        allowable_methods=['GET'],
+        stale_if_error=True,
+    )
+
+    md_resp = session.get(API_METADATA_URL)
+    assert md_resp.status_code == 200
+    all_kc_paths = jq_index_prog.input_value(json.loads(md_resp.text)).all()
+
+    for kc_path in all_kc_paths:
+        kc_resp = session.get(API_URL + "/constants" + kc_path)
+        assert kc_resp.status_code == 200
+        qmk_dict_transfomed = jq_prog.input_value(json.loads(kc_resp.text))
+        for qmk_kc in iter(qmk_dict_transfomed):
+            if 'label' in qmk_kc:
+                kc_dict[qmk_kc['alias']] = "\n\n\n\n\n\n\n\n\n" + str(qmk_kc['label']) + "\n\n"
+
     return kc_dict
 
 
@@ -84,8 +105,6 @@ def main():
                         help='Name of the QMK layer to use (0-based)')
     parser.add_argument('-d', '--dict',
                         help='Additional dictionary file to use (see dict-sample.json')
-    parser.add_argument("-q", "--qmk-firmware-root",
-                        help='Root directory of QMK firmware source tree for reading the keycodes')
 
     # Parse arguments
     args = vars(parser.parse_args())
@@ -99,8 +118,9 @@ def main():
     layer_it = iter(layer)
 
     kc_dict = {}
-    if args['qmk_firmware_root']:
-        load_qmk_keycodes(args['qmk_firmware_root'], kc_dict)
+
+    load_qmk_keycodes_api(kc_dict)
+
     if args['dict']:
         load_custom_dict(args['dict'], kc_dict)
 
