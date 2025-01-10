4b78f3322f8281123c385eb02dc1a4907b5c8a63
Nikolai Grigoriev <2364632+ngrigoriev@users.noreply.github.com>
2025-01-10 11:30:52 -0500

    additional layers and parameters

--- keyboards/handwired/dactyl_manuform/5x7_2_6/keymaps/northstar/extras/make_all_json.sh
+++ keyboards/handwired/dactyl_manuform/5x7_2_6/keymaps/northstar/extras/make_all_json.sh
@@ -10,11 +10,14 @@ LAYERS=(\
     _SYS \
     _TYPING \
     _GAMING \
-    _WORLD)
+    _WORLD \
+    _EMAIL \
+    _SYMBOL \
+    _MOUSE)
 
 lindex=0
 for l in $LAYERS; do
     echo $l
-    python3 gen_kle_layout.py  --dict override_dict.tsv layers.json KLE.json $lindex > $lindex$l.json
+    python3 gen_kle_layout.py --keyboard-name "$lindex$l" --dict override_dict.tsv layers.json KLE.json $lindex > $lindex$l.json
     lindex=$((lindex + 1))
 done
